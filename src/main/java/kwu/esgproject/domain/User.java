69995cc9e0b97e5a3f18dc61bccbebbbcaf9a2e2
jaemin-shin02 <sour_jam0220@naver.com>
2023-08-07 16:10:29 +0900

    h2 db table명 오류 해결 user->users

--- src/main/java/kwu/esgproject/domain/User.java
+++ src/main/java/kwu/esgproject/domain/User.java
@@ -14,6 +14,7 @@ import java.util.List;
 @Getter
 @Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "users")//h2 db는 user이 예약어라 table 오류 발생
 public class User {
     @Id @GeneratedValue
     @Column(name = "user_id")
